13a222664b97f1a6645bfce4a46a583bb90703a5
Timur Dian Radha Sejati <timur7dian84@gmail.com>
2022-06-12 11:54:16 +0700

    Initial

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const adminRouter = require('./routes/admin');
+
+const authRouter = require('./routes/auth');
+const productRouter = require('./routes/product');
+const userRouter = require('./routes/user');
+
+
+// Init
+const PORT = 3000;
+const app = express();
+const DB = "mongodb+srv://radhasjt:Cadenza97256@cluster0.mqrtu.mongodb.net/amazonclone_db?retryWrites=true&w=majority"
+
+// Middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+// Connection database
+mongoose.connect(DB).then(() => {
+	console.log('Connection Successful');
+}).catch((e) => {
+	console.log(e);
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+	console.log(`Connected at port ${PORT}`);
+});
